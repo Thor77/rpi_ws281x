e15b70b3f12a6133ec9dc2e9865ec0d5ddf6000c
Thor77 <thor77@thor77.org>
2017-01-15 16:21:35 +0100

    Fix slice-algorithm in get/set_color
    using wrong attribute for pixel-count

--- python/neopixel3.py
+++ python/neopixel3.py
@@ -133,7 +133,7 @@ class NeoPixel(object):
         if isinstance(position, slice):
             positions_slice = position
             for index, position in enumerate(
-                    range(*positions_slice.indices(self.size))):
+                    range(*positions_slice.indices(self.pixel_count))):
                 ws.ws2811_led_set(self._channel, position, color.value)
         else:
             return ws.ws2811_led_set(self._channel, position, color.value)
@@ -162,7 +162,9 @@ class NeoPixel(object):
             positions_slice = position
             return [
                 ws.ws2811_led_get(self._channel, position)
-                for position in range(*positions_slice.indices(self.size))
+                for position in range(
+                    *positions_slice.indices(self.pixel_count)
+                )
             ]
         else:
             return ws.ws2811_led_get(self._channel, position)
